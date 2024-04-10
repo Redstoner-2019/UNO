26136d3846bba69ee158cf8ef19f6c702f53127d
Lukas Paepke <l.paepke@optadata-gruppe.de>
2024-04-10 09:29:17 +0200

    done nothing

--- src/main/java/me/redstoner2019/main/Main.java
+++ src/main/java/me/redstoner2019/main/Main.java
@@ -17,7 +17,7 @@ import java.util.HashMap;
 import static me.redstoner2019.main.data.CardColor.*;
 
 public class Main {
-    public static final boolean TEST_MODE = true;
+    public static final boolean TEST_MODE = false;
     private static final String VERSION = "v1.5.0-alpha.1";
     public static String username = "";
     public static String password = "";
